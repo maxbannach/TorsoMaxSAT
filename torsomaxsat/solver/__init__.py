8e6c8cf0d14575aa2cc7e6a0b61a399e2b605cf1
bannach <bannach@tcs.uni-luebeck.de>
2023-10-31 10:19:28 +0100

    starting solver class

---
+++ torsomaxsat/solver/__init__.py
@@ -0,0 +1,15 @@
+class Solver():
+
+    def __init__(self, wcnf):
+        self.wcnf       = wcnf  # The input formula (a torsomaxsat WCNF).
+        self.fitness    = 0     # The value of the best model (after run solve).
+        self.assignment = []    # An assignment (of the internal representation of) the wcnf after solver.
+        
+    def solve(self):
+        pass
+
+    def get_fitness(self):
+        return self.fitness + self.wcnf.offset
+
+    def get_cost(self):
+        return self.wcnf._max_fitness() - self.fitness + self.wcnf.offset
---
+++ torsomaxsat/solver/_gurobi.py
@@ -0,0 +1,47 @@
+from torsomaxsat import Solver
+
+import gurobipy as gp
+from gurobipy import GRB
+
+class GurobiSolver(Solver):
+
+    def solve(self):
+
+        # hide Gurobi messages
+        env = gp.Env(empty=True)
+        env.setParam("OutputFlag",0)
+        env.start()
+
+        # build a Gurobi model
+        gurobi = gp.Model(env=env)
+
+        # adding the variables
+        ilp_vars = [0]
+        for i in range(1,self.wcnf.n+1):
+            ilp_vars.append( gurobi.addVar(vtype = GRB.BINARY, name = format(f"x{i}")) )
+        
+        # adding the hard clauses
+        for c in self.wcnf.hard:
+            bound  = 1
+            clause = gp.LinExpr()
+            for l in c:        
+                if l < 0:
+                    clause += -1 * ilp_vars[abs(l)]
+                    bound  -= 1
+                else:
+                    clause += ilp_vars[abs(l)]
+            gurobi.addConstr( clause >= bound )
+
+        # adding the soft clauses
+        obj    = gp.LinExpr()
+        for v in self.wcnf.soft:
+            obj += ilp_vars[v] * self.wcnf.soft[v]
+        gurobi.setObjective(obj, GRB.MAXIMIZE)
+        
+        # solving the model
+        gurobi.optimize()
+
+        # store computed results
+        self.fitness    = gurobi.objVal
+        self.assignment = list(map(lambda v: int(v.X), ilp_vars[1:]))
+    
